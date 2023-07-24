8e9893d684dcb1654236783a328b97c18b64c883
MIDKINGIELIA <qkrrlqja7876@naver.com>
2023-07-24 14:42:38 +0900

    Motor Test1

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,26 @@
 #include <Arduino.h>
+#define BAT_CHECK 4
+#define FLOW_485 5
+#define POWER_CTR 7
+#define LED_DATA 6
+#define MOTOR_CTR 12
 
-// put function declarations here:
-int myFunction(int, int);
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  pinMode(FLOW_485,OUTPUT);
+  pinMode(POWER_CTR,OUTPUT);
+
+
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  tone(MOTOR_CTR,500,500);
+  delay(500);
+  tone(MOTOR_CTR,600,500);
+  delay(500);
+  tone(MOTOR_CTR,700,500);
+  delay(500);
+  tone(MOTOR_CTR,800,500);
+  delay(500);
 }
